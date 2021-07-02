4733e4f488166f48a9d11fe0a043c1af4ca4fa9d
Joao Pedro <joaopbbezerra@gmail.com>
2021-07-02 16:54:59 +0100

    details

--- routes/movies.js
+++ routes/movies.js
@@ -18,23 +18,29 @@ const imdb = require('imdb-api')
 
 router.get("/movies-search", async (req, res)=>{
     const {title} = req.query
-    console.log("title",title)
+    // console.log("title",title)
     const searchResult = await imdb.search({name: title}, {apiKey: '94f5077f', timeout: 30000})
     res.render("movie/movies-search", {searchResult})
 })
 
 router.post("/movies-search", async (req, res)=>{
-    const {title} = req.body
-    console.log("title post",title)
-    const searchResult =  await imdb.search({name: title}, {apiKey: process.env.imdbKey, timeout: 30000})    
+    const {title} = req.body //Pegando do form
+    // console.log("title post",title)
+    const searchResult =  await imdb.search({name: title}, {apiKey: process.env.imdbKey, timeout: 30000})     //Usando o título pra pesquisar na API (método de pesquisa pré feito)
     console.log(searchResult)
-    res.render("movie/movies-search", {searchResult})
+    res.render("movie/movies-search", {searchResult}) //devolve o searchResult (lista de resultados)
 })
 
-router.get("/movies-list", (req, res)=>{ //connecting api
-    res.render("movie/movies-list")
-})
+// router.get("/movies-list", (req, res)=>{ //connecting api
+//     res.render("movie/movies-list")
+// })
 
+router.get("/movies-details/:movieImdbid", async (req, res)=>{
+    const movieDetails =  await imdb.get({id: req.params.movieImdbid}, {apiKey: process.env.imdbKey, timeout: 30000})
+    // res.render("albums", {albums: albumResult.body.items})
+    console.log(movieDetails)
+    res.render("movie/movies-details", {movieDetails})
+})
 
 
 module.exports = router;
