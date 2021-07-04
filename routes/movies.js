78786edf18fc9d3f5930bdd9f9455000b35a098a
Joao Pedro <joaopbbezerra@gmail.com>
2021-07-04 13:21:04 +0100

    Matches working fine, no more exceptions

--- routes/movies.js
+++ routes/movies.js
@@ -49,62 +49,83 @@ router.post("/movies-details/:movieImdbid", async (req, res)=>{
     res.redirect("/movie/movies-details")
 })
 
+
+
+//Adicionar filmes, checar/fazer matches
+
 router.post("/favorites/:moviesId/add", async (req, res)=>{
 try{
     // console.log("favorites", req.session.currentUser.favorites)
     // console.log("movie id", req.params.moviesId)
     
     let counterSameMovies = 0
-    for (let i = 0; i<req.session.currentUser.favorites.length; i++){
-        console.log(req.session.currentUser.favorites[i])
-        if (req.session.currentUser.favorites[i]===req.params.moviesId){
-            counterSameMovies++
-        }
-    }
-    if (counterSameMovies === 0){
+    if (req.session.currentUser.favorites.length < 1){
         await User.findByIdAndUpdate(req.session.currentUser._id, {
-            $push: {favorites: req.params.moviesId}
+            $push: {favorites: req.params.moviesId}  
         })
-        console.log("The movies was pushed ", req.params.moviesId)
+        // res.redirect(`/movies-details/${req.params.moviesId}`)
     } else {
-        console.log("No movies were pushed")
-    }
-    //Achando o primeiro usuário
-    let userNow = await User.findById(req.session.currentUser._id);
-    console.log("First user: ", userNow.favorites)
-    //Achando o segundo usuário a partir do primeiro
-    let userOneDate = await User.findOne({username: userNow.date})
-    console.log("Second user: ",userOneDate.favorites)
-    let repeatedMatches = 0
-    //For pra checar se já está dentro - Tentativa de evitar nested for
-    for (let i = 0; i<userOneDate.matches.length; i++){
-        if (req.params.moviesId === userOneDate.matches[i]){
-            repeatedMatches++
-        }
-    }
-    console.log("Repeated Matches: ",repeatedMatches)
-    if (repeatedMatches !== 0){
-        console.log("Não foi adicionado! Repeated Matches: ", repeatedMatches)
-    }
-    else {
-        //For pra checar o match
-        for (let i=0; i<userOneDate.favorites.length; i++){
-            if (req.params.moviesId === userOneDate.favorites[i]){
-                //Usuário logado
-                await User.findByIdAndUpdate(req.session.currentUser._id, {
-                    $push: {matches: req.params.moviesId}
-                })
-                //Date do usuário
-                await User.findOneAndUpdate({username: userNow.date}, {
-                    $push: {matches: req.params.moviesId}
-                })
-                console.log("Added in both users")
-                
+        for (let i = 0; i<req.session.currentUser.favorites.length; i++){
+            console.log(req.session.currentUser.favorites[i])
+            if (req.session.currentUser.favorites[i]===req.params.moviesId){
+                counterSameMovies++
             }
         }
+        if (counterSameMovies === 0){
+            await User.findByIdAndUpdate(req.session.currentUser._id, {
+                $push: {favorites: req.params.moviesId}
+            })
+            console.log("The movies was pushed ", req.params.moviesId)
+        } else {
+            console.log("No movies were pushed")
+        }
+    }
+        //Achando o primeiro usuário
+    let userNow = await User.findById(req.session.currentUser._id);
+    console.log("First user: ", userNow.favorites)
+
+    //Achando o segundo usuário a partir do primeiro
+    
+    let userOneDate = await User.findOne({username: userNow.date})
+    console.log("Checando se há o date: ", userOneDate)
+    //Checando se há um date pro usuário
+    if (userOneDate){
+        let repeatedMatches = 0
+        //For pra checar se já está dentro - Tentativa de evitar nested for
+            for (let i = 0; i<userOneDate.matches.length; i++){
+                if (req.params.moviesId === userOneDate.matches[i]){
+                    repeatedMatches++
+                }
+            }
+        console.log("Repeated Matches: ",repeatedMatches)
+        if (repeatedMatches !== 0){
+            console.log("Não foi adicionado! Repeated Matches: ", repeatedMatches)
+        }
+        else {
+            //For pra checar o match
+                for (let i=0; i<userOneDate.favorites.length; i++){
+                    console.log("Checando se entrou no for pra testar se o filme tá nos favoritos do date: ", userOneDate.favorites[i])
+                    if (req.params.moviesId === userOneDate.favorites[i]){
+                        //Usuário logado
+                        await User.findByIdAndUpdate(req.session.currentUser._id, {
+                            $push: {matches: req.params.moviesId}
+                        })
+                        //Date do usuário
+                        await User.findOneAndUpdate({username: userNow.date}, {
+                            $push: {matches: req.params.moviesId}
+                        })
+                        console.log("Added in both users") 
+                    }
+                }    
+        }
+        console.log("Second user: ",userOneDate)
+        res.redirect(`/movies-details/${req.params.moviesId}`)
+    }
+    else {
+        console.log("Não foi true")
+        res.redirect(`/movies-details/${req.params.moviesId}`)
     }
 
-    res.redirect(`/movies-details/${req.params.moviesId}`)
 } catch(e){
     req.session.destroy()
     res.redirect("/login")
@@ -112,6 +133,19 @@ try{
 }
 })
 
+router.post("/movies-details/:movieImdbid/delete", async (req, res) => {
+    try{
+        await User.findByIdAndUpdate(req.session.currentUser._id,{
+            $pull: {favorites: req.params.moviesImdbid}
+        });
+        res.redirect("/movie/movies-details");
+    } catch (e){
+        req.session.destroy()
+        res.redirect("/login")
+        console.log(e)
+    }
 
+    
+  });
 
 module.exports = router;
